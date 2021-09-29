6606285f6e04b87d877c67c9824a9be9cad9b76d
Stéphan DROC <stephan.droc@gmail.com>
2021-09-29 21:49:19 +0200

    Module 3

--- api/src/router.js
+++ api/src/router.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
 const multer = require("multer");
+const path = require("path");
 
+const photoPath = path.resolve(__dirname, "../../client/photo-viewer.html");
 const router = Router();
 
 const filename = (request, file, callback) => {
@@ -32,6 +34,10 @@ router.post("/upload", upload.single("photo"), (request, response) => {
   } else {
     response.status(201).json({ success: true });
   }
+});
+
+router.get("/photo-viewer", (request, response) => {
+  response.sendFile(photoPath);
 });
 
 module.exports = router;
